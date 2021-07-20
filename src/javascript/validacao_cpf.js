c91c8866febb4eb34aecb77f6be9d2dfed42b707
Marcella Furtado <mnfurtado@yahoo.com.br>
2021-07-20 19:23:59 -0300

    first commit

---
+++ src/javascript/validacao_cpf.js
@@ -0,0 +1,65 @@
+function verificarCPF() {
+    limpar();
+    let cpf = document.getElementById("cpfInput").value;
+    let soma = 0;
+    let resto = 0;
+    let status = "";
+
+    if (cpf.length === 0) {
+        return document.getElementById("resultadoValidacao").innerHTML = "Digite um número de CPF!";
+    }
+
+    if (cpf === "00000000000") {
+        document.getElementById("resultadoValidacao").innerHTML = "Este CPF <u>não existe!</u>";
+        status = 'errado';
+        imagemValidacao(status);
+        return;
+    }
+
+    for (let i = 0; i < 9; i++) {
+        soma = soma + parseInt(cpf[i] * (10 - i), 10);
+    }
+
+    resto = (soma * 10) % 11;
+
+    if (resto === 10 || resto === 11) resto = 0;
+
+    if (resto !== parseInt(cpf[9], 10)) {
+        document.getElementById("resultadoValidacao").innerHTML = "Este CPF <u>não existe!</u>";
+        status = 'errado';
+        imagemValidacao(status);
+        return;
+    }
+
+    soma = 0;
+
+    for (let i = 0; i < 10; i++) {
+        soma = soma + parseInt(cpf[i] * (11 - i), 10);
+    }
+
+    resto = (soma * 10) % 11;
+
+    if (resto === 10 || resto === 11) resto = 0;
+
+    if (resto !== parseInt(cpf[10], 10)) {
+        document.getElementById("resultadoValidacao").innerHTML = "Este CPF <u>não existe!</u>";
+        status = 'errado';
+        imagemValidacao(status);
+    } else {
+        document.getElementById("resultadoValidacao").innerHTML = "Este CPF é <b>válido</b>.";
+        status = 'certo';
+        imagemValidacao(status);
+    }
+}
+
+function imagemValidacao(status) {
+    const div = document.getElementById("imagemValidacao");
+    const image = document.createElement("img");
+    image.src = `assets/images/${status}.png`;
+    div.appendChild(image);
+}
+
+function limpar() {
+    document.getElementById("imagemValidacao").innerHTML = ""
+}
+
